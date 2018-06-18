7778a0f10fdf9a0ba257959bfe84ab42c8981aaa
github username <github email address>
2018-06-18 16:37:53 -0400

    battles can be created.

--- app/controllers/api/v1/battles_controller.rb
+++ app/controllers/api/v1/battles_controller.rb
@@ -13,6 +13,10 @@ class Api::V1::BattlesController < ApplicationController
 
   def create
     @battle = Battle.create(battle_params)
+    @user1 = User.find_by(id: params[:users][0][:id])
+    @user2 = User.find_by(id: params[:users][1][:id])
+
+    @battle.users = [@user1, @user2]
     if @battle.save
       render json: @battle
     else
@@ -23,7 +27,7 @@ class Api::V1::BattlesController < ApplicationController
   private
 
   def battle_params
-    params.require(:battle).permit(:category)
+    params.permit(:category)
   end
 
 end
